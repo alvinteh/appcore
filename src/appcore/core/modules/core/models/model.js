3fdc3b34d741536eaef072149477963bb062fb6c
alvinteh <alvin.teh@onesolved.com>
2014-03-18 16:37:27 +0800

    Implemented model validation.

--- src/appcore/core/modules/core/models/model.js
+++ src/appcore/core/modules/core/models/model.js
@@ -311,10 +311,110 @@ define(function(require) {
 
                 Validates the current item.
 
-                @return {boolean}
+                @return {boolean||object[]}
             */
             prototype.validate = function() {
+                var modelRules = staticAttributes.validationRules;
+                var modelErrors = {};
+                var attributeRules, attributeRequired, attributeErrors, attributeOverride, attributeValue, rule;
 
+                function addError(message) {
+                    attributeErrors.push(rule.message || message);
+                }
+
+                for (var attribute in modelRules) {
+                    attributeRules = modelRules[attribute];
+                    attributeValue = this[_getAttributes](_key)[attribute];
+                    attributeErrors = [];
+                    attributeOverride = false;
+                    attributeRequired = false;
+
+                    //Set override flag to allow nulls/etc. for non-required attributes with type checks
+                    for (var i = 0, length = attributeRules.length; i < length; i++) {
+                        if (attributeRules[i].required === true) {
+                            attributeRequired = true;
+                            break;
+                        }
+                    }
+                    attributeOverride = (!attributeRequired && (attributeValue === null ||
+                        attributeValue === undefined || attributeValue === ""));
+
+                    for (i = 0, length = attributeRules.length; i < length; i++) {
+                        rule = attributeRules[i];
+
+                        if (rule.required) {
+                            if (attributeValue === null || attributeValue === undefined || attributeValue === "") {
+                                addError(attribute + " is required.");
+                            }
+                        }
+                        else if (rule.type) {
+                            if (!attributeOverride) {
+                                if (rule.type === "string" && typeof attributeValue !== "string") {
+                                    addError(attribute + " should be a string.");
+                                }
+                                else if (rule.type === "int" && (typeof attributeValue !== "number" ||
+                                    attributeValue % 1 !== 0)) {
+                                    addError(attribute + " should be an integer.");
+                                }
+                                else if (rule.type === "number" && typeof attributeValue !== "number") {
+                                    addError(attribute + " should be a number.");
+                                }
+                                else if (rule.type === "boolean" && typeof attributeValue !== "boolean") {
+                                    addError(attribute + " should be a boolean.");
+                                }
+                            }
+                        }
+                        else if (rule.minLength) {
+                            if (attributeValue.length < rule.minLength) {
+                                addError(attribute + " should be at least " + rule.minLength + " characters long.");
+                            }
+                        }
+                        else if (rule.maxLength) {
+                            if (attributeValue.length > rule.maxLength) {
+                                addError(attribute + " should be at most " + rule.maxLength + " characters long.");
+                            }
+                        }
+                        else if (rule.minValue) {
+                            if (attributeValue < rule.minValue) {
+                                addError(attribute + " should be at least " + rule.minValue + ".");
+                            }
+                        }
+                        else if (rule.maxValue) {
+                            if (attributeValue > rule.maxValue) {
+                                addError(attribute + " should be at most " + rule.maxValue + ".");
+                            }
+                        }
+                        else if (rule.format) {
+                            if (
+                                //jshint -W101
+                                (rule.format === "email" && !/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(attributeValue)) ||
+                                (rule.format === "alpha" && !/^[a-z]$/i.test(attributeValue)) ||
+                                (rule.format === "alphanumeric" && !/^[a-z0-9]$/i.test(attributeValue)) ||
+                                (rule.format === "creditcard" && !/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(attributeValue))
+                                //jshint +W101
+                                ) {
+                                addError(attribute + " should be a valid " + rule.format);
+                            }
+                        }
+                        else if (rule.regex) {
+                            if (!rule.regex.test(attributeValue)) {
+                                addError(attribute + " should match " + rule.regex + ".");
+                            }
+                        }
+                        else if (rule.func) {
+                            if (!rule.func(attributeValue, this)) {
+                                addError(attribute + " should pass " + rule.func + ".");
+                            }
+                        }
+                    }
+
+                    if (attributeErrors.length > 0) {
+                        modelErrors[attribute] = attributeErrors;
+                    }
+
+                }
+
+                return (modelErrors.length === 0 || modelErrors);
             };
 
             return prototype;
