366d3ef8374ffaedd1e09484f723bfa175d16863
alvinteh <alvin.teh@onesolved.com>
2014-03-18 11:47:19 +0800

    Sorted test code.

--- test/app.js
+++ test/app.js
@@ -13,37 +13,13 @@ require([
         "appcore/core/modules/util/helpers/uuid-helper"
     ],
     function(Ac, Promise, EventHelper) {
-    /*
-    //Define User class
-    var User = Ac.Model.create(["name", "age"], function(name) {
-        this.set("name", name);
-        this.set("age", 10);
-    });
-
-    User.addMethod("greet", function() {
-        console.log("Hello, I am " + this.getName() + " and I am " + this.getAge() + "!");
-    });
-
-    User.addStaticMethod("test", function() {
-        console.log("Testing class");
-    });
-
-    User.test();
-
-    //Define view
-    var view1 = Ac.View.create("#input-text");
-    var view2 = Ac.View.create("#header-title, #footer");
-
-    //Test user class
-    var p1 = new User("Alan");
-    p1.greet();
-
-    view1.addDataBinding(p1, function() { return p1.getName() + "!!!"; }, "value");
-    view2.addDataBinding(p1, "name", "innerHTML");
-    p1.setName("Bob");
-    */
 
     /*
+     *  PROMISES
+     */
+
+    /*
+    //Promise creation (method #1)
     var testX = Promise.convert(function(callback, error, value) {
         setTimeout(function() {
             value = 10;
@@ -52,6 +28,7 @@ require([
         }, 0);
     });
 
+    //Promise creation (method #2)
     function testY1(value) {
         return new Promise(function(resolve, reject) {
             setTimeout(function() {
@@ -82,7 +59,7 @@ require([
         });
     }
 
-
+    //Test usage
     testX()
     .then(testY1)
     .then(testY2)
@@ -90,12 +67,20 @@ require([
     .then(function(value) {
         alert("Final value = " + value);
     });
-    */
 
     /*
-    console.log(Ac.Helper.get("Uuid").generateUuid());
-    */
+     *  HELPERS
+     */
 
+    /*
+    //Test usage
+    console.log(Ac.Helper.get("Uuid").generateUuid());
+
+    /*
+     *  MODELS
+     */
+
+    //Model definition
     var User = Ac.Model.create(["name", "age"], function(name, age) {
         this.set({
             name: name,
@@ -103,6 +88,36 @@ require([
         });
     });
 
+    //Model method definition
+    User.addMethod("greet", function() {
+        console.log("Hello, I am " + this.getName() + " and I am " + this.getAge() + "!");
+    });
+
+    //Model static method definition
+    User.addStaticMethod("test", function() {
+        console.log("Testing the User class");
+    });
+
+    //Test usage
+    var userA = new User("Alan", 12);
+    var userB = new User("Bob", 15);
+
+    /*
+    userA.greet();
+    userB.greet();
+    userA.set("name", "Adrian");
+    userB.set({ name: "Bill" });
+    userA.greet();
+    userB.greet();
+    User.test();
+    */
+
+    /*
+     * EVENTS
+     */
+
+    /*
+    //Event triggers
     User.addMethod("grow", function() {
         this.set("age", this.get("age") + 1);
         this.trigger("grow", { age: this.get("age") });
@@ -112,9 +127,7 @@ require([
         this.trigger("talk", { message: message });
     });
 
-    var userA = new User("Alan", 12);
-    var userB = new User("Bob", 15);
-
+    //Event observers
     EventHelper.observe(userA, "grow", function(event) {
         console.log(event.getTarget().getName() + " has grown to " + event.getTarget().getAge() + " years old.");
     });
@@ -127,6 +140,7 @@ require([
         console.log(event.getTarget().getName() + " has grown to " + event.getTarget().getAge() + " years old.");
     });
 
+    //Test usage
     userA.grow();
     userA.grow();
     userA.grow();
@@ -134,6 +148,22 @@ require([
     userB.grow();
     userA.talk("Sup bro?");
 
+    /*
+     * DATA BINDING
+     */
+
+    /*
+    //Define view
+    var view1 = Ac.View.create("#input-text");
+    var view2 = Ac.View.create("#header-title, #footer");
+
+    //Data binding creation
+    view1.addDataBinding(userA, function() { return userA.getName() + "!!!"; }, "value");
+    view2.addDataBinding(userA, "name", "innerHTML");
+
+    //Test usage
+    userA.setName("Aaron");
+    */
 });
 
 /*
