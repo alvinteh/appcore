e5330d723f4c801a8e2f93d49663d7e8827b9958
Alvin Teh <alvin.teh@outlook.com>
2014-06-20 06:23:05 -0700

    Test: updated tests for the framework.

--- test/specs/framework.js
+++ test/specs/framework.js
@@ -1,12 +1,14 @@
-define(function(require) {
+define(function() {
     var Am = window.Am;
 
-    describe("Am framework", function() {
+    describe("Am", function() {
+
         it("should be defined.", function(done) {
             expect(Am).to.exist;
 
             done();
         });
+
     });
 
 });
