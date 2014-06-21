993af234b5b3b35c75e865e70039b31a1d4efef0
Alvin Teh <alvin.teh@outlook.com>
2014-06-21 08:59:30 -0700

    Test: rewrote tests for Util.ModelHelper, Util.StringHelper and Util.UuidHelper for efficiency.

--- test/specs/util/string_helper.js
+++ test/specs/util/string_helper.js
@@ -1,93 +1,65 @@
 define(function() {
     var Am = window.Am;
 
-    describe("Am", function() {
-        describe("Util", function() {
-            describe("StringHelper", function() {
+    Am.Module.load("Util").then(function() {
+        var StringHelper = Am.Module.get("Util").StringHelper;
 
-                it("should be defined.", function(done) {
-                    Am.Module.load("Util").then(function() {
-                        var StringHelper = Am.Module.get("Util").StringHelper;
+        describe("Am", function() {
+            describe("Util", function() {
+                describe("StringHelper", function() {
 
+                    it("should be defined.", function(done) {
                         expect(StringHelper).to.exist;
 
                         done();
                     });
-                });
 
-                describe("convertToCamelCase()", function() {
-                    it("should convert snake-cased strings to camelCase", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var StringHelper = Am.Module.get("Util").StringHelper;
-
+                    describe("convertToCamelCase()", function() {
+                        it("should convert snake-cased strings to camelCase", function(done) {
                             expect(StringHelper.convertToCamelCase("test_string_here")).to.equal("testStringHere");
 
                             done();
                         });
                     });
-                });
 
-                describe("convertToSnakeCase()", function() {
-                    it("should convert camelCased strings to snake_case", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var StringHelper = Am.Module.get("Util").StringHelper;
-
+                    describe("convertToSnakeCase()", function() {
+                        it("should convert camelCased strings to snake_case", function(done) {
                             expect(StringHelper.convertToSnakeCase("testStringHere")).to.equal("test_string_here");
 
                             done();
                         });
                     });
-                });
 
-                describe("lowercaseFirst()", function() {
-                    it("should convert the first character of the string to lowercase", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var StringHelper = Am.Module.get("Util").StringHelper;
-
-                            var test1 = StringHelper.lowercaseFirst("TestStringHere") === "testStringHere";
-                            var test2 = StringHelper.lowercaseFirst("testStringHere") === "testStringHere";
-                            var test3 = StringHelper.lowercaseFirst("TEstStringHere") === "tEstStringHere";
-                            var test4 = StringHelper.lowercaseFirst("tEstStringHere") === "tEstStringHere";
-
-                            expect(test1 && test2 && test3 && test4).to.be.true;
+                    describe("lowercaseFirst()", function() {
+                        it("should convert the first character of the string to lowercase", function(done) {
+                            expect(StringHelper.lowercaseFirst("testStringHere")).to.equal("testStringHere");
+                            expect(StringHelper.lowercaseFirst("TestStringHere")).to.equal("testStringHere");
+                            expect(StringHelper.lowercaseFirst("tEstStringHere")).to.equal("tEstStringHere");
+                            expect(StringHelper.lowercaseFirst("TEstStringHere")).to.equal("tEstStringHere");
 
                             done();
                         });
                     });
-                });
 
-                describe("uppercaseFirst()", function() {
-                    it("should convert the first character of the string to uppercase", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var StringHelper = Am.Module.get("Util").StringHelper;
-
-                            var test1 = StringHelper.uppercaseFirst("TestStringHere") === "TestStringHere";
-                            var test2 = StringHelper.uppercaseFirst("testStringHere") === "TestStringHere";
-                            var test3 = StringHelper.uppercaseFirst("TEstStringHere") === "TEstStringHere";
-                            var test4 = StringHelper.uppercaseFirst("tEstStringHere") === "TEstStringHere";
-
-                            expect(test1 && test2 && test3 && test4).to.be.true;
+                    describe("uppercaseFirst()", function() {
+                        it("should convert the first character of the string to uppercase", function(done) {
+                            expect(StringHelper.uppercaseFirst("testStringHere")).to.equal("TestStringHere");
+                            expect(StringHelper.uppercaseFirst("TestStringHere")).to.equal("TestStringHere");
+                            expect(StringHelper.uppercaseFirst("tEstStringHere")).to.equal("TEstStringHere");
+                            expect(StringHelper.uppercaseFirst("TEstStringHere")).to.equal("TEstStringHere");
 
                             done();
                         });
                     });
-                });
 
-                describe("humanize()", function() {
-                    it("should humanize camel-cased strings", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var StringHelper = Am.Module.get("Util").StringHelper;
-
+                    describe("humanize()", function() {
+                        it("should humanize camel-cased strings", function(done) {
                             expect(StringHelper.humanize("testStringHere")).to.equal("test string here");
 
                             done();
                         });
-                    });
 
-                    it("should humanize snake-cased strings", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var StringHelper = Am.Module.get("Util").StringHelper;
-
+                        it("should humanize snake-cased strings", function(done) {
                             expect(StringHelper.humanize("test_string_here")).to.equal("test string here");
 
                             done();
--- test/specs/util/uuid_helper.js
+++ test/specs/util/uuid_helper.js
@@ -1,44 +1,30 @@
 define(function() {
     var Am = window.Am;
 
-    describe("Am", function() {
-        describe("Util", function() {
-            describe("UuidHelper", function() {
+    Am.Module.load("Util").then(function() {
+        var UuidHelper = Am.Module.get("Util").UuidHelper;
 
-                it("should be defined.", function(done) {
-                    Am.Module.load("Util").then(function() {
-                        var UuidHelper = Am.Module.get("Util").UuidHelper;
+        describe("Am", function() {
+            describe("Util", function() {
+                describe("UuidHelper", function() {
 
+                    it("should be defined.", function(done) {
                         expect(UuidHelper).to.exist;
 
                         done();
                     });
-                });
 
-                describe("generateUuid()", function() {
-                    it("should generate a random string", function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var UuidHelper = Am.Module.get("Util").UuidHelper;
-
-                            var uuid1 = UuidHelper.generateUuid();
-                            var uuid2 = UuidHelper.generateUuid();
-
-                            expect(uuid1).to.not.equal(uuid2);
+                    describe("generateUuid()", function() {
+                        it("should generate a random string", function(done) {
+                            expect(UuidHelper.generateUuid()).to.not.equal(UuidHelper.generateUuid());
 
                             done();
                         });
-                    });
 
-                    it("should keep dashes if specified",
-                        function(done) {
-                        Am.Module.load("Util").then(function() {
-                            var UuidHelper = Am.Module.get("Util").UuidHelper;
-
-                            var test1 = UuidHelper.generateUuid().indexOf("-") === -1;
-                            var test2 = UuidHelper.generateUuid(true).indexOf("-") === -1;
-                            var test3 = UuidHelper.generateUuid(false).indexOf("-") !== -1;
-
-                            expect(test1 && test2 && test3).to.be.true;
+                        it("should keep dashes if specified", function(done) {
+                            expect(UuidHelper.generateUuid().indexOf("-")).to.deep.equal(-1);
+                            expect(UuidHelper.generateUuid(true).indexOf("-")).to.deep.equal(-1);
+                            expect(UuidHelper.generateUuid(false).indexOf("-")).to.not.deep.equal(-1);
 
                             done();
                         });
