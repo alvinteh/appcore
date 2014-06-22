f5504aeeea6d5495674ee6878498dc974cc8e32b
Alvin Teh <alvin.teh@outlook.com>
2014-06-22 00:01:03 -0700

    Test: implemented tests for Util.StorageHelper.

--- test/specs.js
+++ test/specs.js
@@ -2,6 +2,7 @@ define([
     "specs/framework.js",
     "specs/util/function_helper.js",
     "specs/util/model_helper.js",
+    "specs/util/storage_helper.js",
     "specs/util/string_helper.js",
     "specs/util/uuid_helper.js"
 ]);
