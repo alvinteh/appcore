cacb955caa98b761471b94c9fe7d05e55d6e192c
Alvin Teh <alvin.teh@outlook.com>
2014-07-10 01:03:13 +0800

    Test: added additional tests for Core.View.Element.

--- test/specs/core/models/element.js
+++ test/specs/core/models/element.js
@@ -115,6 +115,148 @@ define(function() {
                         done();
                     });
 
+                    it("should bind the instance to the Element's HTMLElement(s)' characterData by default for " +
+                        "non-input HTML  elements", function(done) {
+                        $("#test").append("<div id=\"test1\">");
+                        $("#test").append("<div id=\"test2\">");
+                        $("#test").append("<div id=\"test3\">");
+                        var Person = Am.Model.create("Person", ["name"]);
+                        var person = new Person("Alan");
+                        var element1 = Am.View.Element.create("#test1");
+                        var element2 = Am.View.Element.create("#test2, #test3");
+
+                        element1.addDataBinding(person, "name");
+                        element2.addDataBinding(person, "name");
+                        $("#test1").html("John");
+
+                        window.setTimeout(function() {
+                            expect(person.get("name")).to.equal("John");
+
+                            //element2.refresh();
+
+                            //expect($("#test2").html()).to.equal("John");
+                            //expect($("#test3").html()).to.equal("John");
+
+                            done();
+                        }, 0);
+                    });
+
+                    it("should bind the instance to the Element's HTMLElement(s)' value by default for input " +
+                        "HTML  elements", function(done) {
+                        $("#test").append("<input id=\"test1\" type=\"text\">");
+                        $("#test").append("<input id=\"test2\" type=\"text\">");
+                        $("#test").append("<input id=\"test3\" type=\"text\">");
+                        var Person = Am.Model.create("Person", ["name"]);
+                        var person = new Person("Alan");
+                        var element1 = Am.View.Element.create("#test1");
+                        var element2 = Am.View.Element.create("#test2, #test3");
+
+                        element1.addDataBinding(person, "name");
+                        element2.addDataBinding(person, "name");
+                        $("#test1").val("John");
+
+                        //Manually trigger change event
+                        var event = document.createEvent("HTMLEvents");
+                        event.initEvent("change", true, true);
+                        $("#test1")[0].dispatchEvent(event);
+
+                        window.setTimeout(function() {
+                            expect(person.get("name")).to.equal("John");
+
+                            //element2.refresh();
+
+                            //expect($("#test2").html()).to.equal("John");
+                            //expect($("#test3").html()).to.equal("John");
+
+                            done();
+                        }, 0);
+                    });
+
+                    it("should respect the backward transform option on characterData if it is specified",
+                        function(done) {
+                        $("#test").append("<div id=\"test1\">");
+                        var Person = Am.Model.create("Person", ["name"]);
+                        var person = new Person("Alan");
+                        var element = Am.View.Element.create("#test1");
+
+                        element.addDataBinding(person, "name", {
+                            backwardTransform: function(value) { return value.toLowerCase(); }
+                        });
+                        $("#test1").html("John");
+
+                        window.setTimeout(function() {
+                            expect(person.get("name")).to.equal("john");
+                            done();
+                        }, 50);
+                    });
+
+                    it("should respect the backward transform option on attributes if it is specified",
+                        function(done) {
+                        var assertionCount = 2;
+                        var completedCount = 0;
+                        var notify = function() {
+                            if (++completedCount === assertionCount) {
+                                done();
+                            }
+                        };
+
+                        $("#test").append("<div id=\"test1\">");
+                        var Color = Am.Model.create("Color", ["name"]);
+                        var color = new Color("Red");
+                        var element1 = Am.View.Element.create("#test1");
+
+                        element1.addDataBinding(color, "name", {
+                            elementProperties: ["data-color"],
+                            backwardTransform: function(value) { return value.toString().toLowerCase(); }
+                        });
+                        $("#test1")[0].dataset.color = "Blue";
+
+                        window.setTimeout(function() {
+                            expect(color.get("name")).to.equal("blue");
+                            notify();
+                        }, 0);
+
+                        $("#test").append("<input id=\"test2\" type=\"text\">");
+                        var Input = Am.Model.create("Input", ["type"]);
+                        var input = new Input("TEXT");
+                        var element2 = Am.View.Element.create("#test2");
+
+                        element2.addDataBinding(input, "type", {
+                            elementProperties: ["type"],
+                            backwardTransform: function(value) { return value.toString().toUpperCase(); }
+                        });
+
+                        $("#test2")[0].type = "email";
+
+                        window.setTimeout(function() {
+                            expect(input.get("type")).to.equal("EMAIL");
+                            notify();
+                        }, 0);
+                    });
+
+                    it("should respect the backward transform option on input values if it is specified",
+                        function(done) {
+                        $("#test").append("<input id=\"test1\" type=\"text\">");
+                        var Person = Am.Model.create("Person", ["name"]);
+                        var person = new Person("Alan");
+                        var element = Am.View.Element.create("#test1");
+
+                        element.addDataBinding(person, "name", {
+                            backwardTransform: function(value) { return value.toLowerCase(); }
+                        });
+                        $("#test1").val("John");
+
+                        //Manually trigger change event
+                        var event = document.createEvent("HTMLEvents");
+                        event.initEvent("change", true, true);
+                        $("#test1")[0].dispatchEvent(event);
+
+                        window.setTimeout(function() {
+                            expect(person.get("name")).to.equal("john");
+                            done();
+                        }, 50);
+                    });
+
                     it("should respect the direction option if it is specified, or default to two-way binding",
                         function(done) {
                         $("#test").append("<input id=\"test1\" type=\"text\">");
